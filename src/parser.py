a57f44587629def269f702f407c8a358822b307d
hashi0203 <n.hsmt1123@gmail.com>
2021-09-04 17:19:24 +0900

    convert dict to list (seqences and packets)

--- src/parser.py
+++ src/parser.py
@@ -5,16 +5,13 @@ def readfile(file_name, ext='.txt', path='../data'):
     data = f.readlines()
     f.close()
 
-    ret = {}
-    ret[0] = {}
+    ret = [{}]
     stack = [ret, ret[0]]
-    hidx = 1
-    pidx = 0
 
     for d in data:
         d = d.strip()
         if d.endswith(':'):
-            stack[-1][d[:-1]] = {}
+            stack[-1][d[:-1]] = [] if d.startswith("packets") else {}
             stack.append(stack[-1][d[:-1]])
         elif d.startswith("frame_id"):
             l = d.split()
@@ -25,9 +22,8 @@ def readfile(file_name, ext='.txt', path='../data'):
             stack[-1][l[0][:-1]] = int(l[1])
             stack = stack[:-1]
         elif d == '-':
-            stack[-1][pidx] = {}
-            stack.append(stack[-1][pidx])
-            pidx += 1
+            stack[-1].append({})
+            stack.append(stack[-1][-1])
         elif d.startswith("data"):
             l = d.split()
             v = [int(l[1][1:-1])] + [int(k[:-1]) for k in l[2:]]
@@ -35,20 +31,22 @@ def readfile(file_name, ext='.txt', path='../data'):
             stack = stack[:-1]
         elif d == "---":
             stack = stack[:-2]
-            stack[-1][hidx] = {}
-            stack.append(stack[-1][hidx])
-            hidx += 1
+            stack[-1].append({})
+            stack.append(stack[-1][-1])
         else:
             l = d.split()
             stack[-1][l[0][:-1]] = int(l[1])
-    return ret
+    return ret[:-1]
 
 
 if __name__ == '__main__':
-    data = readfile('parking-lot')
-    print(data[0]["header"])
-    print(len(data))
-    print(len(data[0]["packets"]))
+    # data[number of sequences]["header"]["seq" / "stamp" / "frame_id"]
+    # data[number of sequences]["packets"][number of packet data]["stamp" / "data"]
 
-    # data[hidx]["header"]["seq" / "stamp" / "frame_id"]
-    # data[hidx]["packets"][pidx]["stamp" / "data"]
+    data = readfile('parking-lot')
+    print(len(data)) # number of sequences: 602 (parking-lot), 600 (urban-road, residential-area)
+    print(len(data[0]["packets"])) # number of packet data: 348
+    print(data[0]["header"])
+    print(data[1]["header"])
+    print(data[2]["header"])
+    print(data[-1]["header"])

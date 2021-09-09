53ab82cc50a228e836785b8fbed93d51d1d920b1
hashi0203 <n.hsmt1123@gmail.com>
2021-09-09 09:22:13 +0900

    parse data as well

--- src/parser.py
+++ src/parser.py
@@ -1,6 +1,37 @@
 import os
 
-def readfile(file_name, ext='.txt', path='../data'):
+def concat_uint8(l):
+    ret = 0
+    for i in range(len(l)):
+        ret |= l[i] << (i << 3)
+    return ret
+
+
+def parseData(data):
+    assert len(data) == 1206
+
+    ret = { "blocks" : [] }
+    for block_head in range(0, 1200, 100):
+        fring = data[block_head : block_head + 100]
+        block = {}
+        block["id"] = concat_uint8(fring[0:2])
+        block["pos"] = concat_uint8(fring[2:4]) / 100.0
+        block["lasers"] = []
+        for laser_head in range(4, 100, 3):
+            laser = {}
+            laser["dist"] = concat_uint8(fring[laser_head : laser_head + 2])
+            laser["intensity"] = fring[laser_head + 2]
+            block["lasers"].append(laser)
+        ret["blocks"].append(block)
+
+    ret["stamp"] = concat_uint8(data[-6:-2])
+    ret["type"] = data[-2]
+    ret["value"] = data[-1]
+
+    return ret
+
+
+def readFile(file_name, ext='.txt', path='../data'):
     f = open(os.path.join(path, file_name+ext), 'r')
     data = f.readlines()
     f.close()
@@ -27,7 +58,7 @@ def readfile(file_name, ext='.txt', path='../data'):
         elif d.startswith("data"):
             l = d.split()
             v = [int(l[1][1:-1])] + [int(k[:-1]) for k in l[2:]]
-            stack[-1][l[0][:-1]] = v
+            stack[-1][l[0][:-1]] = parseData(v)
             stack = stack[:-1]
         elif d == "---":
             stack = stack[:-2]
@@ -36,14 +67,20 @@ def readfile(file_name, ext='.txt', path='../data'):
         else:
             l = d.split()
             stack[-1][l[0][:-1]] = int(l[1])
+
     return ret[:-1]
 
 
 if __name__ == '__main__':
     # data[number of sequences]["header"]["seq" / "stamp" / "frame_id"]
     # data[number of sequences]["packets"][number of packet data]["stamp" / "data"]
+    # data[][][]["data"]["blocks"][number of blocks (=12)]["id" / "pos" / "lasers"]
+    # data[][][][][][]["lasers"][number of lasers (=32)]["dist" / "intensity"]
+    # data[][][]["data"]["stamp" / "type" / "value"]
 
-    data = readfile('parking-lot')
+    data = readFile('parking-lot')
+    # data = readFile('urban-road')
+    # data = readFile('residential-area')
     print(len(data)) # number of sequences: 602 (parking-lot), 600 (urban-road, residential-area)
     print(len(data[0]["packets"])) # number of packet data: 348
     print(data[0]["header"])

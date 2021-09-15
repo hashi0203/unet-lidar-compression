d2facf0f57524edff6547a327e2e33dd5d050d2d
hashi0203 <n.hsmt1123@gmail.com>
2021-09-15 16:33:19 +0900

    update config

--- src/config.py
+++ src/config.py
@@ -2,3 +2,7 @@ data_path = '../data'
 data_name = ['parking-lot', 'urban-road', 'residential-area']
 yaml_name = '64S2'
 mu, theta = 11.128, 9.671
+nbframe = 3
+nepoch = 2
+CKPT_PATH = '../checkpoint'
+GRAPH_PATH = '../graph'

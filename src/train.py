258abc57da99ee3baca2297e29d1164dc8424461
hashi0203 <n.hsmt1123@gmail.com>
2021-10-07 23:50:03 +0900

    update arg comment

--- src/train.py
+++ src/train.py
@@ -21,7 +21,7 @@ print = functools.partial(print, flush=True)
 
 parser = argparse.ArgumentParser(description='PyTorch LiDAR Point Cloud Compression Training')
 # --progress, -p: use progress bar when preparing dataset
-parser.add_argument('--progress', '-p', action='store_true', help='use progress bar')
+parser.add_argument('--progress', '-p', action='store_true', help='show progress bar')
 # --refresh, -r: refresh dataset files to update
 parser.add_argument('--refresh', '-r', action='store_true', help='refresh dataset files')
 # --summary, -s: show torchsummary to see the neural model structure
